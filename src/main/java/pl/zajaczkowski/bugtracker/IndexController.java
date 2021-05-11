653030d544f8d50114269dd80b8edac54eccc0ae
Paweł Zajączkowski <pzdev2@gmail.com>
2021-05-11 11:08:25 +0200

    change authority name in controller

--- src/main/java/pl/zajaczkowski/bugtracker/IndexController.java
+++ src/main/java/pl/zajaczkowski/bugtracker/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/dashboard")
-    @Secured("ROLE_USER_TAB")
+    @Secured("ROLE_USERS_TAB")
     String dashboard() {
         return "dashboard";
     }
