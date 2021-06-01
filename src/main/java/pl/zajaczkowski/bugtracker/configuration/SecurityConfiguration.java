709e3f693330eb26022c8226d46909cdc652b1a5
pzdev2 <pzdev2@gmail.com>
2021-06-01 09:12:12 +0200

    remove test for project API

--- src/main/java/pl/zajaczkowski/bugtracker/configuration/SecurityConfiguration.java
+++ src/main/java/pl/zajaczkowski/bugtracker/configuration/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/about", "/api/projects/**").permitAll()
+                .antMatchers("/", "/about").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login")
                 .defaultSuccessUrl("/projects", true).permitAll()
